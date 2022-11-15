a4c7df77d8f2a72b8185a083b467d8c7f0cfe1eb
LukasKubicek <lukaskubicek@gmail.com>
2022-11-15 15:48:40 +0100

    example solution

---
+++ Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmptyBlazorApp1.Models
+{
+    public class LoginAccountForm
+    {
+        [Required(ErrorMessage = "Provide email.")]
+        [EmailAddress(ErrorMessage = "Email not valid.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password requiered.")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Models/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmptyBlazorApp1.Models
+{
+    public class RegisterAccountForm
+    {
+        [Required(ErrorMessage = "Email requiered")]
+        [EmailAddress(ErrorMessage = "Email not valid.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Passwod requiered")]
+        [StringLength(30, ErrorMessage = "Password min length is 8.", MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Password again")]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+        public string Password2 { get; set; }
+
+    }
+}
